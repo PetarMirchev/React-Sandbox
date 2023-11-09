e8b133f12ae614e21280dc714db46eed71252f14
Petar Mirchev <petar.g.mirchev@gmail.com>
2023-11-09 20:44:41 +0200

    form box - Uncontrolled Form example
    Uncontrolled Form example sandbox - JS

---
+++ forms/src/App.js
@@ -0,0 +1,16 @@
+import ControlledFormRow from "./components/ControlledFormRow";
+import UncontrolledFormExample from "./components/UncontrolledFormExample";
+
+
+function App() {
+
+
+  return (
+    <div>
+      <UncontrolledFormExample/>
+      {/* <ControlledFormRow/> */}
+    </div>
+  );
+}
+
+export default App;
